2405326a46c14abb340b619871265f2650873095
Caterina <cateilario@gmail.com>
2023-12-08 18:36:01 +0100

    minor changes and project finished

--- scripts/deleteProduct.js
+++ scripts/deleteProduct.js
@@ -2,7 +2,7 @@ import { productManager } from "./main.js";
 import { updateInventoryTable } from "./updateTable.js";
 
 export const deleteProduct = product => {
-    if(confirm(`¿Seguro que seas borrar ${product.name}?`)) {
+    if(confirm(`¿Seguro que seas borrar el producto "${product.name}"?`)) {
         productManager.deleteProductByID(product.id);
         updateInventoryTable();
     }  
